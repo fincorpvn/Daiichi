bc61e58949b78af6cd10931daed2442ec1719d11
tanthanhvnc <73580525+tanthanhvnc@users.noreply.github.com>
2022-07-11 14:08:03 +0700

    sa
    fix ios

--- iTRUST/iTRUST/src/components/HTMLView.tsx
+++ iTRUST/iTRUST/src/components/HTMLView.tsx
@@ -1,8 +1,8 @@
-import {Div} from 'components';
-import {Ecolors, Efonts} from 'constant';
+import { Div } from 'components';
+import { Ecolors, Efonts } from 'constant';
 import React from 'react';
 import RenderHtml from 'react-native-render-html';
-import {fontScale, heightScale, widthScale, widthScreen} from 'utils';
+import { fontScale, heightScale, widthScale, widthScreen } from 'utils';
 function HTMLView(Props: {
   source: {
     html: string;
@@ -18,21 +18,27 @@ function HTMLView(Props: {
           fontSize: fontScale(16),
           fontFamily: Efonts.medium,
           paddingHorizontal: widthScale(15),
+          fontWeight: '400'
         },
         h3: {
           lineHeight: heightScale(18),
           fontSize: fontScale(15),
           fontFamily: Efonts.medium,
+          fontWeight: '400'
+
         },
         p: {
           lineHeight: heightScale(18),
           fontSize: fontScale(15),
           fontFamily: Efonts.medium,
+          fontWeight: '400'
+
         },
         strong: {
           lineHeight: heightScale(18),
           fontFamily: Efonts.bold,
           fontSize: fontScale(15),
+          fontWeight: '700'
         },
       }}
     />

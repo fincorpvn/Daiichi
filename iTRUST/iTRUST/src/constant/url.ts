68dc1da2ef893ce52984cdf92eaf23a5a86d2a97
tanthanhvnc <73580525+tanthanhvnc@users.noreply.github.com>
2022-08-01 08:33:40 +0700

    sdas
    build ios

--- iTRUST/iTRUST/src/constant/url.ts
+++ iTRUST/iTRUST/src/constant/url.ts
@@ -1,6 +1,6 @@
 export enum urlApp {
-  APIURL = 'https://apis.fplatform.fincorp.vn/web/v1/', //test//
-  // APIURL = 'https://oapis.fplatform-uat.fincorp.vn/web/v1/', //uat//
+  // APIURL = 'https://apis.fplatform.fincorp.vn/web/v1/', //test//
+  APIURL = 'https://oapis.fplatform-uat.fincorp.vn/web/v1/', //uat//
   // APIURL = 'https://oapis.fplatform-live.fincorp.vn/web/v1/', //live//
   DomainName = 'https://daiichi.mobile', // moi doi tac sua origin daichi
   // DomainName = 'https://fplatform.mobile', // moi doi tac sua origin fplatform
